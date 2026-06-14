515e39fa2b072731fb2234a4ff1fd10ec58e74c7
justforwangzhi-del <justforwangzhi@gmail.com>
2026-06-14 21:10:52 +0800

    fix: 修复数据保存问题，添加localStorage双保险

--- sw.js
+++ sw.js
@@ -1,13 +1,7 @@
-const CACHE_NAME='today-v3';
-const BASE='https://justforwangzhi-del.github.io/today-assistant/';
-const urlsToCache=[BASE,BASE+'index.html'];
+const CACHE_NAME='today-v4';
 
 self.addEventListener('install',e=>{
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache=>cache.addAll(urlsToCache))
-    .then(()=>self.skipWaiting())
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate',e=>{
@@ -19,11 +13,14 @@ self.addEventListener('activate',e=>{
 });
 
 self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
   e.respondWith(
     fetch(e.request)
     .then(res=>{
-      const resClone=res.clone();
-      caches.open(CACHE_NAME).then(c=>c.put(e.request,resClone));
+      if(res.ok){
+        const resClone=res.clone();
+        caches.open(CACHE_NAME).then(c=>c.put(e.request,resClone));
+      }
       return res;
     })
     .catch(()=>caches.match(e.request))
