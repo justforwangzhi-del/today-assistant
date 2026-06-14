62cd055ff420863002f79204a730e13bfd8dd0f7
justforwangzhi-del <justforwangzhi@gmail.com>
2026-06-14 15:26:02 +0800

    v4: 修复PWA完整路径

--- sw.js
+++ sw.js
@@ -1,32 +1,31 @@
-const CACHE_NAME='today-assistant-v2';
-const urlsToCache=['./','./index.html'];
+const CACHE_NAME='today-v3';
+const BASE='https://justforwangzhi-del.github.io/today-assistant/';
+const urlsToCache=[BASE,BASE+'index.html'];
 
 self.addEventListener('install',e=>{
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+    .then(cache=>cache.addAll(urlsToCache))
     .then(()=>self.skipWaiting())
   );
 });
 
 self.addEventListener('activate',e=>{
   e.waitUntil(
-    caches.keys().then(names=>Promise.all(
-      names.filter(n=>n!==CACHE_NAME).map(n=>caches.delete(n))
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
     )).then(()=>self.clients.claim())
   );
 });
 
 self.addEventListener('fetch',e=>{
   e.respondWith(
-    caches.match(e.request).then(response=>{
-      return response||fetch(e.request).then(fetchRes=>{
-        return caches.open(CACHE_NAME).then(cache=>{
-          cache.put(e.request,fetchRes.clone());
-          return fetchRes;
-        });
-      });
-    }).catch(()=>{
-      return caches.match('./index.html');
+    fetch(e.request)
+    .then(res=>{
+      const resClone=res.clone();
+      caches.open(CACHE_NAME).then(c=>c.put(e.request,resClone));
+      return res;
     })
+    .catch(()=>caches.match(e.request))
   );
 });
