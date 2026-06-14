88514fda3faf0e3c053daf9e3f3ca5b168eb5b5a
justforwangzhi-del <justforwangzhi@gmail.com>
2026-06-14 15:12:08 +0800

    修复PWA路径问题

--- sw.js
+++ sw.js
@@ -1,14 +1,32 @@
-const CACHE_NAME='today-assistant-v1';
-const urlsToCache=['/index.html'];
+const CACHE_NAME='today-assistant-v2';
+const urlsToCache=['./','./index.html'];
 
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
-});
-
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(response=>response||fetch(e.request)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
+    .then(()=>self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(names=>Promise.all(names.filter(n=>n!==CACHE_NAME).map(n=>caches.delete(n)))));
+  e.waitUntil(
+    caches.keys().then(names=>Promise.all(
+      names.filter(n=>n!==CACHE_NAME).map(n=>caches.delete(n))
+    )).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(response=>{
+      return response||fetch(e.request).then(fetchRes=>{
+        return caches.open(CACHE_NAME).then(cache=>{
+          cache.put(e.request,fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    }).catch(()=>{
+      return caches.match('./index.html');
+    })
+  );
 });
